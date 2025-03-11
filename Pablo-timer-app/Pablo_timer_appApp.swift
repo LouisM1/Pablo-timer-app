b72a1bcd64a72b0e2ba6626cd65bd90aafd9ba66
LouisM1 <louismcauliffe@icloud.com>
2025-03-11 18:09:42 +0000

    Initial Commit

---
+++ Pablo-timer-app/Pablo_timer_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pablo_timer_appApp.swift
+//  Pablo-timer-app
+//
+//  Created by Louis McAuliffe on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pablo_timer_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pablo-timer-appTests/Pablo_timer_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pablo_timer_appTests.swift
+//  Pablo-timer-appTests
+//
+//  Created by Louis McAuliffe on 11/03/2025.
+//
+
+import Testing
+@testable import Pablo_timer_app
+
+struct Pablo_timer_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
